2f97a9da9408f91a9625d03c100e982c2f2fe409
Tuancoder1801 <tuantd1801@gmail.com>
2025-03-26 16:39:18 +0700

    update

---
+++ Assets/Scripts/Character/PlayerEquipment.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEquipment : MonoBehaviour
+{
+    public Animator anim;
+
+    public Transform tranHat;
+    public Transform tranGlass;
+    public Transform tranBag;
+}
